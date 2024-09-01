dfc5c27896b73dfce1d17fd465d5d91b77cf21cd
eneserciyes <m.eneserciyes@gmail.com>
2024-09-01 00:10:41 -0400

    customizations on omakub

--- install/desktop/set-emojis.sh
+++ install/desktop/set-emojis.sh
@@ -1,3 +1,5 @@
-cp ~/.local/share/omakub/configs/xcompose ~/.XCompose
-ibus restart
-gsettings set org.gnome.desktop.input-sources xkb-options "['compose:caps']"
+# cp ~/.local/share/omakub/configs/xcompose ~/.XCompose
+# ibus restart
+# gsettings set org.gnome.desktop.input-sources xkb-options "['compose:caps']"
+
+# TODO: instead of these, do the caps lock as ctrl here
